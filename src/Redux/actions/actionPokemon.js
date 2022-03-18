da4ec07c83d00dc79a9398a021f8ed95103de483
jefferson garcia <jdgarcia988@misena.edu.co>
2022-03-18 00:44:58 -0500

    finalizado

--- src/Redux/actions/actionPokemon.js
+++ src/Redux/actions/actionPokemon.js
@@ -1,4 +1,4 @@
-import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, query, updateDoc, where } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDocs, query, updateDoc, where } from "firebase/firestore";
 import { db } from "../../firebase/firebaseConfig";
 import { typesPokemon } from "../types/type";
 
